ee3ac9c3c061578de8c149667f1391965d9daa98
Muhammad Umer <mumer.29199@gmail.com>
2026-03-11 02:31:36 -0400

    Delete BTP500-LABS-Farhan directory
    This was Farhan's own personla work

--- BTP500-LABS-Farhan/BTP500-LAB1-fuction3-Farhan.py
+++
@@ -1,9 +0,0 @@
-def function3(list):
- n = len(list)                # 1
- for i in range(n - 1):       # (n - 1) + 1 
-  for j in range(n - 1 - i):  # (n - 1 - i) + 1
-   if list[j] > list[j+1]:    # (n - 1 - i) comparisons
-      tmp = list[j]           # (n - 1 - i) 
-      list[j] = list[j+1]     # (n - 1 - i) 
-      list[j + 1] = tmp       # (n - 1 - i) 
-      
